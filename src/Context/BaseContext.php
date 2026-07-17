12b167242f289feea0a1dc559e0fe48c771f2e79
Clément JOBEILI <cjobeili@campings.com>
2026-07-17 16:27:58 +0200

    chore: attributes for behat 4

--- src/Context/BaseContext.php
+++ src/Context/BaseContext.php
@@ -4,6 +4,7 @@ namespace Behatch\Context;
 
 use Behat\Behat\Context\TranslatableContext;
 use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\Transformation\Transform;
 
 abstract class BaseContext extends RawMinkContext implements TranslatableContext
 {
@@ -15,19 +16,12 @@ abstract class BaseContext extends RawMinkContext implements TranslatableContext
         return glob(__DIR__ . '/../../i18n/*.xliff') ?: [];
     }
 
-    /**
-     * en
-     * @transform /^(0|[1-9]\d*)(?:st|nd|rd|th)?$/
-     *
-     * fr
-     * @transform /^(0|[1-9]\d*)(?:ier|er|e|ème)?$/
-     *
-     * pt
-     * @transform /^(0|[1-9]\d*)º?$/
-     *
-     * ru
-     * @transform /^(0|[1-9]\d*)(?:ой|ий|ый|ей|й)?$/
-     */
+    // Behat 4 dropped @transform docblock annotations in favour of the #[Transform] attribute.
+    // One pattern per supported locale (en / fr / pt / ru) casting ordinals/numbers to int.
+    #[Transform('/^(0|[1-9]\d*)(?:st|nd|rd|th)?$/')]
+    #[Transform('/^(0|[1-9]\d*)(?:ier|er|e|ème)?$/')]
+    #[Transform('/^(0|[1-9]\d*)º?$/')]
+    #[Transform('/^(0|[1-9]\d*)(?:ой|ий|ый|ей|й)?$/')]
     public function castToInt($count)
     {
         if (intval($count) < PHP_INT_MAX) {
